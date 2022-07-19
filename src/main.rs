deec63ea2e23d5bcb67e9e57d9eb6b9e91736a42
chris <chris.kind.man@gmail.com>
2022-07-19 23:32:39 +0100

    Add example use of horned-owl

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use horned_owl::command::parse_path;
+use std::path::Path;
+use std::rc::Rc;
+use horned_owl::model::{Axiom, Build, Class, ClassAssertion, ClassExpression, DeclareClass, DeclareNamedIndividual, DisjointClasses, DisjointUnion, EquivalentClasses, EquivalentObjectProperties, NamedIndividual, ObjectProperty, ObjectPropertyDomain, ObjectPropertyExpression, SubObjectPropertyExpression, SubObjectPropertyOf, TransitiveObjectProperty, Individual as OWLIndividual, ObjectPropertyAssertion};
+use horned_owl::model::ClassExpression::ObjectUnionOf;
+use horned_owl::ontology::set::SetOntology;
+
+use im::{HashSet, hashset};
+
+
+pub fn print_expression(expr: &ClassExpression) {
+    println!("{:?}", expr); 
+}
+
+pub fn print_ontology(ontology: &SetOntology) {
+
+    for ann_axiom in ontology.iter() {
+        let axiom = &ann_axiom.axiom;
+        match axiom {
+             Axiom::SubClassOf(ax) => { println!("SubClassOf:");
+                 print_expression(&ax.sub);
+                 print_expression(&ax.sup); 
+             },
+             _ => println!("Something else"),
+        };
+    }
+}
+
+fn main() {
+
+    match parse_path(Path::new("ontology.owl")) {
+        Ok(parsed) => {
+                let ont = &parsed.decompose().0;
+                print_ontology(ont);
+        }
+        Err(error) => { dbg!("ERROR: {}", error); }
+    } 
+}
