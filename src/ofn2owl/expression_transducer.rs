4f6ffb4189f4fd39818041bb6ff83ab3423b14e9
chris <chris.kind.man@gmail.com>
2022-08-25 23:45:30 +0100

    Finish ClassExpression translation

--- src/ofn2owl/expression_transducer.rs
+++ src/ofn2owl/expression_transducer.rs
@@ -33,39 +33,97 @@ pub fn translate_class_expression(v : &Value) -> ClassExpression {
 
          Some("ObjectSomeValuesFrom") => translate_object_some_values_from(v), 
          Some("ObjectAllValuesFrom") => translate_object_all_values_from(v), 
-         //Some("ObjectHasValue") => translate_has_value(v), 
-         //Some("ObjectMinCardinality") => translate_min_cardinality(v), 
-         //Some("ObjectMinQualifiedCardinality") => translate_min_qualified_cardinality(v), 
-         //Some("ObjectMaxCardinality") => translate_max_cardinality(v), 
-         //Some("ObjectMaxQualifiedCardinality") => translate_max_qualified_cardinality(v), 
-         //Some("ObjectExactCardinality") => translate_exact_cardinality(v), 
-         //Some("ObjectExactQualifiedCardinality") => translate_exact_qualified_cardinality(v), 
-         //Some("ObjectHasSelf") => translate_has_self(v), 
-         //Some("ObjectIntersectionOf") => translate_intersection_of(v), 
-         //Some("ObjectUnionOf") => translate_union_of(v), 
-         //Some("ObjectOneOf") => translate_one_of(v), 
-         //Some("ObjectComplementOf") => translate_complement_of(v), 
-         //Some("ObjectInverseOf") => property_translation::translate_inverse_of(v), 
+         Some("ObjectHasValue") => translate_object_has_value(v), 
+         Some("ObjectMinCardinality") => translate_object_min_cardinality(v), 
+         Some("ObjectMinQualifiedCardinality") => translate_object_min_qualified_cardinality(v), 
+         Some("ObjectMaxCardinality") => translate_object_max_cardinality(v), 
+         Some("ObjectMaxQualifiedCardinality") => translate_object_max_qualified_cardinality(v), 
+         Some("ObjectExactCardinality") => translate_object_exact_cardinality(v), 
+         Some("ObjectExactQualifiedCardinality") => translate_object_exact_qualified_cardinality(v), 
+         Some("ObjectHasSelf") => translate_object_has_self(v), 
+         Some("ObjectIntersectionOf") => translate_object_intersection_of(v), 
+         Some("ObjectUnionOf") => translate_object_union_of(v), 
+         Some("ObjectOneOf") => translate_object_one_of(v), 
+         Some("ObjectComplementOf") => translate_object_complement_of(v), 
 
-         //Some("DataSomeValuesFrom") => translate_some_values_from(v), 
-         //Some("DataAllValuesFrom") => translate_all_values_from(v), 
-         //Some("DataHasValue") => translate_has_value(v), 
-         //Some("DataMinCardinality") => translate_min_cardinality(v), 
-         //Some("DataMinQualifiedCardinality") => translate_min_qualified_cardinality(v), 
-         //Some("DataMaxCardinality") => translate_max_cardinality(v), 
-         //Some("DataMaxQualifiedCardinality") => translate_max_qualified_cardinality(v), 
-         //Some("DataExactCardinality") => translate_exact_cardinality(v), 
-         //Some("DataExactQualifiedCardinality") => translate_exact_qualified_cardinality(v), 
-         //Some("DataHasSelf") => translate_has_self(v), 
-         //Some("DataIntersectionOf") => translate_intersection_of(v), 
-         //Some("DataUnionOf") => translate_union_of(v), 
-         //Some("DataOneOf") => translate_one_of(v), 
-         //Some("DataComplementOf") => translate_complement_of(v), 
+         Some("DataSomeValuesFrom") => translate_data_some_values_from(v), 
+         Some("DataAllValuesFrom") => translate_data_all_values_from(v), 
+         Some("DataHasValue") => translate_data_has_value(v), 
+         Some("DataMinCardinality") => translate_data_min_cardinality(v), 
+         Some("DataMinQualifiedCardinality") => translate_data_min_qualified_cardinality(v), 
+         Some("DataMaxCardinality") => translate_data_max_cardinality(v), 
+         Some("DataMaxQualifiedCardinality") => translate_data_max_qualified_cardinality(v), 
+         Some("DataExactCardinality") => translate_data_exact_cardinality(v), 
+         Some("DataExactQualifiedCardinality") => translate_data_exact_qualified_cardinality(v), 
 
          Some(_) => panic!(),
          None => translate_named_class(v),
      }
 } 
+
+pub fn translate_literal(v: &Value) -> Literal {
+    //TODO 
+    panic!() 
+}
+
+pub fn translate_data_range(v : &Value) -> DataRange {
+     match v[0].as_str() {
+         Some("Datatype") => translate_datatype(v), 
+         Some("DataIntersectionOf") => translate_data_intersection_of(v), 
+         Some("DataUnionOf") => translate_data_union_of(v), 
+         Some("DataComplementOf") => translate_data_complement_of(v), 
+         Some("DataOneOf") => translate_data_one_of(v), 
+         //TODO
+         //Some("DatatypeRestriction") => translate_object_some_values_from(v), 
+         Some(_) => panic!(),
+         None => panic!(),
+     } 
+}
+
+pub fn translate_data_one_of(v : &Value) -> DataRange { 
+
+    let operands: Vec<Literal> = (&(v.as_array().unwrap())[1..])
+                                               .into_iter()
+                                               .map(|x| translate_literal(&x))
+                                               .collect(); 
+    DataRange::DataOneOf(operands) 
+}
+
+pub fn translate_data_complement_of(v : &Value) -> DataRange { 
+
+    let argument: DataRange = translate_data_range(&v[1]); 
+
+       DataRange::DataComplementOf(Box::new(argument))
+}
+
+pub fn translate_data_intersection_of(v : &Value) -> DataRange {
+    let operands: Vec<DataRange> = (&(v.as_array().unwrap())[1..])
+                                               .into_iter()
+                                               .map(|x| translate_data_range(&x))
+                                               .collect(); 
+
+    DataRange::DataIntersectionOf(operands) 
+}
+
+pub fn translate_data_union_of(v : &Value) -> DataRange {
+    let operands: Vec<DataRange> = (&(v.as_array().unwrap())[1..])
+                                               .into_iter()
+                                               .map(|x| translate_data_range(&x))
+                                               .collect(); 
+
+    DataRange::DataUnionOf(operands) 
+}
+
+pub fn translate_datatype(v : &Value) -> DataRange {
+    let b = Build::new();
+
+    let iri = match v {
+        Value::String(x) => x,
+        _ => panic!("Not a named entity"), 
+    }; 
+    
+    DataRange::Datatype(b.datatype(iri))
+}
 
 pub fn translate_named_object_property(v : &Value) -> ObjectPropertyExpression {
     let b = Build::new();
@@ -76,6 +134,17 @@ pub fn translate_named_object_property(v : &Value) -> ObjectPropertyExpression {
     }; 
 
     b.object_property(iri).into() 
+}
+
+pub fn translate_data_property(v : &Value) -> DataProperty {
+    let b = Build::new();
+
+    let iri = match v {
+        Value::String(x) => x,
+        _ => panic!("Not a named entity"), 
+    }; 
+
+    b.data_property(iri).into() 
 }
 
 pub fn translate_inverse_of(v : &Value) -> ObjectPropertyExpression {
@@ -103,6 +172,20 @@ pub fn translate_named_class(v : &Value) -> ClassExpression {
     b.class(iri).into()
 }
 
+pub fn translate_individual(v : &Value) -> Individual {
+
+    //TODO: handle anonymous individuals
+
+    let b = Build::new();
+
+    let iri = match v {
+        Value::String(x) => x,
+        _ => panic!("Not a named entity"), 
+    }; 
+
+    b.named_individual(iri).into()
+}
+
 pub fn translate_object_some_values_from(v : &Value) -> ClassExpression {
 
     let property = translate_property_expression(&v[1]); 
@@ -122,5 +205,368 @@ pub fn translate_object_all_values_from(v : &Value) -> ClassExpression {
     ClassExpression::ObjectAllValuesFrom {
         ope: property,
         bce : Box::new(filler)
+    } 
+}
+
+pub fn translate_object_has_value(v : &Value) -> ClassExpression {
+
+    let property = translate_property_expression(&v[1]); 
+    let individual: Individual = translate_individual(&v[2]); 
+
+    ClassExpression::ObjectHasValue {
+        ope: property,
+        i : individual,
+    } 
+}
+
+pub fn translate_object_min_cardinality(v : &Value) -> ClassExpression { 
+    let b = Build::new();
+
+    let cardinality = match v[1].clone() {
+        Value::Number(x) => {
+            match x.as_u64() {
+                Some(y) => y,
+                _ => panic!("Not a valid cardinality"), 
+            }
+        }, 
+        _ => panic!("Not a named entity"), 
+    }; 
+
+    let property = translate_property_expression(&v[2]); 
+    let filler: ClassExpression = b.class("http://www.w3.org/2002/07/owl#Thing").into();
+
+    ClassExpression::ObjectMinCardinality {
+        n : cardinality as u32,
+        ope: property,
+        bce : Box::new(filler)
+    } 
+}
+
+pub fn translate_object_min_qualified_cardinality(v : &Value) -> ClassExpression { 
+
+    let cardinality = match v[1].clone() {
+        Value::Number(x) => {
+            match x.as_u64() {
+                Some(y) => y,
+                _ => panic!("Not a valid cardinality"), 
+            }
+        }, 
+        _ => panic!("Not a named entity"), 
+    }; 
+
+    let property = translate_property_expression(&v[2]); 
+    let filler: ClassExpression = translate_class_expression(&v[3]); 
+
+    ClassExpression::ObjectMinCardinality {
+        n : cardinality as u32,
+        ope: property,
+        bce : Box::new(filler)
+    } 
+}
+
+pub fn translate_object_max_cardinality(v : &Value) -> ClassExpression { 
+
+    let b = Build::new();
+
+    let cardinality = match v[1].clone() {
+        Value::Number(x) => {
+            match x.as_u64() {
+                Some(y) => y,
+                _ => panic!("Not a valid cardinality"), 
+            }
+        }, 
+        _ => panic!("Not a named entity"), 
+    }; 
+
+    let property = translate_property_expression(&v[2]); 
+    let filler: ClassExpression = b.class("http://www.w3.org/2002/07/owl#Thing").into();
+
+    ClassExpression::ObjectMaxCardinality {
+        n : cardinality as u32,
+        ope: property,
+        bce : Box::new(filler)
+    } 
+}
+
+pub fn translate_object_max_qualified_cardinality(v : &Value) -> ClassExpression { 
+
+    let cardinality = match v[1].clone() {
+        Value::Number(x) => {
+            match x.as_u64() {
+                Some(y) => y,
+                _ => panic!("Not a valid cardinality"), 
+            }
+        }, 
+        _ => panic!("Not a named entity"), 
+    }; 
+
+    let property = translate_property_expression(&v[2]); 
+    let filler: ClassExpression = translate_class_expression(&v[3]); 
+
+    ClassExpression::ObjectMaxCardinality {
+        n : cardinality as u32,
+        ope: property,
+        bce : Box::new(filler)
+    } 
+}
+
+
+pub fn translate_object_exact_cardinality(v : &Value) -> ClassExpression { 
+
+    let b = Build::new();
+
+    let cardinality = match v[1].clone() {
+        Value::Number(x) => {
+            match x.as_u64() {
+                Some(y) => y,
+                _ => panic!("Not a valid cardinality"), 
+            }
+        }, 
+        _ => panic!("Not a named entity"), 
+    }; 
+
+    let property = translate_property_expression(&v[2]); 
+    let filler: ClassExpression = b.class("http://www.w3.org/2002/07/owl#Thing").into();
+
+    ClassExpression::ObjectExactCardinality {
+        n : cardinality as u32,
+        ope: property,
+        bce : Box::new(filler)
+    } 
+}
+
+pub fn translate_object_exact_qualified_cardinality(v : &Value) -> ClassExpression { 
+
+    let cardinality = match v[1].clone() {
+        Value::Number(x) => {
+            match x.as_u64() {
+                Some(y) => y,
+                _ => panic!("Not a valid cardinality"), 
+            }
+        }, 
+        _ => panic!("Not a named entity"), 
+    }; 
+
+    let property = translate_property_expression(&v[2]); 
+    let filler: ClassExpression = translate_class_expression(&v[3]); 
+
+    ClassExpression::ObjectExactCardinality {
+        n : cardinality as u32,
+        ope: property,
+        bce : Box::new(filler)
+    } 
+}
+
+pub fn translate_object_has_self(v : &Value) -> ClassExpression { 
+
+    let property = translate_property_expression(&v[1]); 
+    ClassExpression::ObjectHasSelf(property) 
+}
+
+pub fn translate_object_intersection_of(v : &Value) -> ClassExpression { 
+
+    let operands: Vec<ClassExpression> = (&(v.as_array().unwrap())[1..])
+                                               .into_iter()
+                                               .map(|x| translate_class_expression(&x))
+                                               .collect(); 
+
+    ClassExpression::ObjectIntersectionOf(operands)
+}
+
+pub fn translate_object_union_of(v : &Value) -> ClassExpression { 
+
+    let operands: Vec<ClassExpression> = (&(v.as_array().unwrap())[1..])
+                                               .into_iter()
+                                               .map(|x| translate_class_expression(&x))
+                                               .collect(); 
+
+    ClassExpression::ObjectUnionOf(operands)
+}
+
+pub fn translate_object_one_of(v : &Value) -> ClassExpression { 
+
+    let operands: Vec<Individual> = (&(v.as_array().unwrap())[1..])
+                                               .into_iter()
+                                               .map(|x| translate_individual(&x))
+                                               .collect(); 
+
+    ClassExpression::ObjectOneOf(operands)
+}
+
+pub fn translate_object_complement_of(v : &Value) -> ClassExpression { 
+
+    let argument: ClassExpression = translate_class_expression(&v[1]); 
+
+       ClassExpression::ObjectComplementOf(Box::new(argument))
+}
+
+pub fn translate_data_some_values_from(v : &Value) -> ClassExpression {
+
+    let property = translate_data_property(&v[1]); 
+    let filler: DataRange = translate_data_range(&v[2]); 
+
+    ClassExpression::DataSomeValuesFrom {
+        dp: property,
+        dr : filler
+    } 
+}
+
+pub fn translate_data_all_values_from(v : &Value) -> ClassExpression {
+
+    let property = translate_data_property(&v[1]); 
+    let filler: DataRange = translate_data_range(&v[2]); 
+
+    ClassExpression::DataAllValuesFrom {
+        dp: property,
+        dr : filler
+    } 
+}
+
+pub fn translate_data_has_value(v : &Value) -> ClassExpression {
+
+    let property = translate_data_property(&v[1]); 
+    let filler: Literal = translate_literal(&v[2]); 
+
+    ClassExpression::DataHasValue {
+        dp: property,
+        l: filler
+    } 
+}
+
+pub fn translate_data_min_cardinality(v : &Value) -> ClassExpression { 
+
+    let b = Build::new();
+
+    let cardinality = match v[1].clone() {
+        Value::Number(x) => {
+            match x.as_u64() {
+                Some(y) => y,
+                _ => panic!("Not a valid cardinality"), 
+            }
+        }, 
+        _ => panic!("Not a named entity"), 
+    }; 
+
+    let property = translate_data_property(&v[2]); 
+    let filler: DataRange = DataRange::Datatype(b.datatype("rdfs:Literal"));
+
+    ClassExpression::DataMinCardinality {
+        n : cardinality as u32,
+        dp: property,
+        dr : filler
+    } 
+}
+
+pub fn translate_data_min_qualified_cardinality(v : &Value) -> ClassExpression { 
+
+    let cardinality = match v[1].clone() {
+        Value::Number(x) => {
+            match x.as_u64() {
+                Some(y) => y,
+                _ => panic!("Not a valid cardinality"), 
+            }
+        }, 
+        _ => panic!("Not a named entity"), 
+    }; 
+
+    let property = translate_data_property(&v[2]); 
+    let filler: DataRange = translate_data_range(&v[3]);
+
+    ClassExpression::DataMinCardinality {
+        n : cardinality as u32,
+        dp: property,
+        dr : filler
+    } 
+}
+
+pub fn translate_data_max_cardinality(v : &Value) -> ClassExpression { 
+
+    let b = Build::new();
+
+    let cardinality = match v[1].clone() {
+        Value::Number(x) => {
+            match x.as_u64() {
+                Some(y) => y,
+                _ => panic!("Not a valid cardinality"), 
+            }
+        }, 
+        _ => panic!("Not a named entity"), 
+    }; 
+
+    let property = translate_data_property(&v[2]); 
+    let filler: DataRange = DataRange::Datatype(b.datatype("rdfs:Literal"));
+
+    ClassExpression::DataMaxCardinality {
+        n : cardinality as u32,
+        dp: property,
+        dr : filler
+    } 
+}
+
+pub fn translate_data_max_qualified_cardinality(v : &Value) -> ClassExpression { 
+
+    let cardinality = match v[1].clone() {
+        Value::Number(x) => {
+            match x.as_u64() {
+                Some(y) => y,
+                _ => panic!("Not a valid cardinality"), 
+            }
+        }, 
+        _ => panic!("Not a named entity"), 
+    }; 
+
+    let property = translate_data_property(&v[2]); 
+    let filler: DataRange = translate_data_range(&v[3]);
+
+    ClassExpression::DataMaxCardinality {
+        n : cardinality as u32,
+        dp: property,
+        dr : filler
+    } 
+}
+
+pub fn translate_data_exact_cardinality(v : &Value) -> ClassExpression { 
+
+    let b = Build::new();
+
+    let cardinality = match v[1].clone() {
+        Value::Number(x) => {
+            match x.as_u64() {
+                Some(y) => y,
+                _ => panic!("Not a valid cardinality"), 
+            }
+        }, 
+        _ => panic!("Not a named entity"), 
+    }; 
+
+    let property = translate_data_property(&v[2]); 
+    let filler: DataRange = DataRange::Datatype(b.datatype("rdfs:Literal"));
+
+    ClassExpression::DataExactCardinality {
+        n : cardinality as u32,
+        dp: property,
+        dr : filler
+    } 
+}
+
+pub fn translate_data_exact_qualified_cardinality(v : &Value) -> ClassExpression { 
+
+    let cardinality = match v[1].clone() {
+        Value::Number(x) => {
+            match x.as_u64() {
+                Some(y) => y,
+                _ => panic!("Not a valid cardinality"), 
+            }
+        }, 
+        _ => panic!("Not a named entity"), 
+    }; 
+
+    let property = translate_data_property(&v[2]); 
+    let filler: DataRange = translate_data_range(&v[3]);
+
+    ClassExpression::DataExactCardinality {
+        n : cardinality as u32,
+        dp: property,
+        dr : filler
     } 
 }
