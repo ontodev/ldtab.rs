c7b3bfb1cd69687776c240854278e4d00698a34c
chris <chris.kind.man@gmail.com>
2022-09-12 21:49:05 +0100

    Set s flag for regex

--- src/ofn2owl/util.rs
+++ src/ofn2owl/util.rs
@@ -2,38 +2,29 @@ use serde_json::{Value};
 use regex::Regex;
 
 pub fn is_literal(v: &Value) -> bool {
-    match v.as_str() {
-        Some(x) => is_literal_string(x),
-        None => false 
+    match v {
+        Value::String(x) => is_literal_string(x),
+        _ => false 
     }
 }
 
 pub fn is_anonynous_individual(v : &Value) -> bool {
-    match v.as_str() {
-        Some(x) => is_literal_string(x),
-        None => false 
-    } 
+    match v {
+        Value::String(x) => is_literal_string(x),
+        _ => false,
+    }
 }
 
 pub fn is_anonymous_individual(s : &str) -> bool {
-    let anonymous = Regex::new("^(.*)_:(.+)$").unwrap();
-    if anonymous.is_match(s) {
-        true
-    } else  {
-        false
-    } 
+    let anonymous = Regex::new("^(.*)_:(.+)$").unwrap(); 
+    anonymous.is_match(s)
 }
 
 pub fn is_literal_string(s : &str) -> bool {
 
-    let literal = Regex::new("^\"(.+)\"(.*)$").unwrap();
-    //let simple = Regex::new("^\"(.+)\"$").unwrap();
-    //let language_tag = Regex::new("^\"(.+)\"@(.*)$").unwrap();
-    //let datatype = Regex::new("^\"(.+)\"\\^\\^(.*)$").unwrap();
+    //NB: "(?s)" sets a flag so that . matches \n 
+    //let literal = Regex::new("^\"(.+)\"(.*)$").unwrap();
+    let literal = Regex::new("(?s)^\"(.+)\"(.*)$").unwrap();
 
-    if literal.is_match(s) {
-        true
-    } else  {
-        false
-    } 
+    literal.is_match(s) 
 }
