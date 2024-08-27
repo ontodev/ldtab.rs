0cd8121484e93612f5951c016af505786fe7c92c
ckindermann <chris.kind.man@gmail.com>
2024-08-26 23:54:21 -0700

    Wrap IRIs in angle brackets

--- src/owl_2_ofn/annotation_transducer.rs
+++ src/owl_2_ofn/annotation_transducer.rs
@@ -1,45 +1,58 @@
-use serde_json::{Value};
-use serde_json::json; 
-use crate::owl_2_ofn::expression_transducer as expression_transducer;
-use horned_owl::model::{Annotation, AnnotationProperty, AnnotationValue, AnnotationSubject, RcStr};
+use crate::owl_2_ofn::expression_transducer;
+use horned_owl::model::{
+    Annotation, AnnotationProperty, AnnotationSubject, AnnotationValue, RcStr,
+};
+use serde_json::json;
+use serde_json::Value;
 use std::collections::BTreeSet;
 
-
-pub fn translate_annotation(annotation : &Annotation<RcStr>) -> Value {
+pub fn translate_annotation(annotation: &Annotation<RcStr>) -> Value {
     let operator = Value::String(String::from("Annotation"));
     let property = translate_annotation_property(&annotation.ap);
     let value = translate_annotation_value(&annotation.av);
-    let res = vec![operator, property, value]; 
-    Value::Array(res) 
+    let res = vec![operator, property, value];
+    Value::Array(res)
 }
 
-pub fn translate_annotation_subject(annotation_subject : &AnnotationSubject<RcStr>) -> Value {
+pub fn translate_annotation_subject(annotation_subject: &AnnotationSubject<RcStr>) -> Value {
     match annotation_subject {
-        AnnotationSubject::IRI(x) => json!(x.get(0..)),
-        AnnotationSubject::AnonymousIndividual(x) => expression_transducer::translate_anonymous_individual(x),
+        AnnotationSubject::IRI(x) => {
+            let i = x.get(0..);
+            let iri = "<".to_string() + i.unwrap() + ">";
+            json!(iri)
+        }
+        AnnotationSubject::AnonymousIndividual(x) => {
+            expression_transducer::translate_anonymous_individual(x)
+        }
     }
 }
 
-pub fn translate_annotation_property(property : &AnnotationProperty<RcStr>) -> Value { 
+pub fn translate_annotation_property(property: &AnnotationProperty<RcStr>) -> Value {
     let a = property.0.get(0..);
-    json!(a)
+    let iri = "<".to_string() + a.unwrap() + ">";
+    json!(iri)
 }
 
-pub fn translate_annotation_value(value : &AnnotationValue<RcStr>) -> Value {
+pub fn translate_annotation_value(value: &AnnotationValue<RcStr>) -> Value {
     match value {
         AnnotationValue::Literal(x) => expression_transducer::translate_literal(x),
-        AnnotationValue::IRI(x) => json!(x.get(0..)),
-        AnnotationValue::AnonymousIndividual(x) =>  expression_transducer::translate_anonymous_individual(x),
-    } 
+        AnnotationValue::IRI(x) => {
+            let i = x.get(0..);
+            let iri = "<".to_string() + i.unwrap() + ">";
+            json!(iri)
+        }
+        AnnotationValue::AnonymousIndividual(x) => {
+            expression_transducer::translate_anonymous_individual(x)
+        }
+    }
 }
 
-
-pub fn translate_annotation_set(annotation_set : &BTreeSet<Annotation<RcStr>>) -> Vec<Value> {
-        let mut res = Vec::new();
-        for annotation in annotation_set {
-            res.push(translate_annotation(annotation));
-        } 
-        res
+pub fn translate_annotation_set(annotation_set: &BTreeSet<Annotation<RcStr>>) -> Vec<Value> {
+    let mut res = Vec::new();
+    for annotation in annotation_set {
+        res.push(translate_annotation(annotation));
+    }
+    res
 }
 
 //pub fn translate_annotation_set(annotation_set : &BTreeSet<Annotation>) -> Value {
@@ -47,6 +60,6 @@ pub fn translate_annotation_set(annotation_set : &BTreeSet<Annotation<RcStr>>) -
 //        let mut res = vec![operator];
 //        for annotation in annotation_set {
 //            res.push(translate_annotation(annotation));
-//        } 
-//        Value::Array(res) 
+//        }
+//        Value::Array(res)
 //}
