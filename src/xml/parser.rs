3186ea1a82b170d52aa0005332585c0d49356635
ckindermann <chris.kind.man@gmail.com>
2026-01-26 22:02:13 -0800

    Update parser

--- src/xml/parser.rs
+++ src/xml/parser.rs
@@ -1,14 +1,13 @@
-use rio_xml::{RdfXmlParser, RdfXmlError};
-use rio_api::parser::TriplesParser;
 use rio_api::model::NamedNode;
+use rio_api::parser::TriplesParser;
+use rio_xml::{RdfXmlError, RdfXmlParser};
 use std::path::Path;
 
-use std::io::BufReader;
 use std::fs::File;
+use std::io::BufReader;
 
 pub fn parse(file: &str) {
-
-    let ex_file = b"<?xml version=\"1.0\"?>
+    let _ex_file = b"<?xml version=\"1.0\"?>
 <rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:schema=\"http://schema.org/\">
  <rdf:Description rdf:about=\"http://example.com/foo\">
    <rdf:type rdf:resource=\"http://schema.org/Person\" />
@@ -19,15 +18,18 @@ pub fn parse(file: &str) {
 
     let path = Path::new(file);
 
-    let rdf_type = NamedNode { iri: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" };
+    let rdf_type = NamedNode {
+        iri: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+    };
     let mut count = 0;
     //RdfXmlParser::new(ex_file.as_ref(), None).parse_all(&mut |t| {
-    RdfXmlParser::new(BufReader::new(File::open(path).unwrap()), None).parse_all(&mut |t| {
-        if t.predicate == rdf_type {
-            count += 1;
-        }
-        Ok(()) as Result<(), RdfXmlError>
-    }).unwrap();
-    println!("Count is {:?}", count); 
-
+    RdfXmlParser::new(BufReader::new(File::open(path).unwrap()), None)
+        .parse_all(&mut |t| {
+            if t.predicate == rdf_type {
+                count += 1;
+            }
+            Ok(()) as Result<(), RdfXmlError>
+        })
+        .unwrap();
+    println!("Count is {:?}", count);
 }
